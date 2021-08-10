e8923e669dcd0063d57172519bf8a0a6aed46166
James D. Trotter <james@simula.no>
2021-08-10 13:09:13 +0200

    Extract function for parsing Matrix Market header

--- libmtx/mtx/header.c
+++ libmtx/mtx/header.c
@@ -24,6 +24,11 @@
 
 #include <libmtx/mtx/header.h>
 
+#include <libmtx/error.h>
+
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * `mtx_object_str()` is a string representing the Matrix Market object
  * type.
@@ -83,4 +88,113 @@ const char * mtx_symmetry_str(
     case mtx_hermitian: return "hermitian";
     default: return "unknown";
     }
+}
+
+/**
+ * `mtx_header_parse()' parses a string containing the header line for
+ * a file in Matrix Market format.
+ *
+ * If `endptr' is not `NULL', then the address stored in `endptr'
+ * points to the first character beyond the characters that were
+ * consumed during parsing.
+ *
+ * On success, `mtx_header_parse()` returns `MTX_SUCCESS' and
+ * `object', `format', `field' and `symmetry' will be set according to
+ * the contents of the parsed Matrix Market header.  Otherwise, an
+ * appropriate error code is returned if the input is not a valid
+ * Matrix Market header.
+ */
+int mtx_header_parse(
+    const char * s,
+    int * bytes_read,
+    const char ** endptr,
+    enum mtx_object * object,
+    enum mtx_format * format,
+    enum mtx_field * field,
+    enum mtx_symmetry * symmetry)
+{
+    int err;
+    const char * t = s;
+
+    *bytes_read = 0;
+    /* Parse the identifier. */
+    if (strncmp("%%MatrixMarket", t, strlen("%%MatrixMarket")) != 0)
+        return MTX_ERR_INVALID_MTX_HEADER;
+    while (*t != '\0' && *t != ' ')
+        t++;
+    while (*t == ' ')
+        t++;
+    *bytes_read = t-s;
+
+    /* Parse the object type. */
+    if (strncmp("matrix ", t, strlen("matrix ")) == 0) {
+        *object = mtx_matrix;
+    } else if (strncmp("vector ", t, strlen("vector ")) == 0) {
+        *object = mtx_vector;
+    } else {
+        return MTX_ERR_INVALID_MTX_OBJECT;
+    }
+    while (*t != '\0' && *t != ' ')
+        t++;
+    while (*t == ' ')
+        t++;
+    *bytes_read = t-s;
+
+    /* Parse the format. */
+    if (strncmp("array ", t, strlen("array ")) == 0) {
+        *format = mtx_array;
+    } else if (strncmp("coordinate ", t, strlen("coordinate ")) == 0) {
+        *format = mtx_coordinate;
+    } else {
+        return MTX_ERR_INVALID_MTX_FORMAT;
+    }
+    while (*t != '\0' && *t != ' ')
+        t++;
+    while (*t == ' ')
+        t++;
+    *bytes_read = t-s;
+
+    /* Parse the field type. */
+    if (strncmp("real ", t, strlen("real ")) == 0) {
+        *field = mtx_real;
+    } else if (strncmp("double ", t, strlen("double ")) == 0) {
+        *field = mtx_double;
+    } else if (strncmp("complex ", t, strlen("complex ")) == 0) {
+        *field = mtx_complex;
+    } else if (strncmp("integer ", t, strlen("integer ")) == 0) {
+        *field = mtx_integer;
+    } else if (strncmp("pattern ", t, strlen("pattern ")) == 0) {
+        *field = mtx_pattern;
+    } else {
+        return MTX_ERR_INVALID_MTX_FIELD;
+    }
+    while (*t != '\0' && *t != ' ')
+        t++;
+    while (*t == ' ')
+        t++;
+    *bytes_read = t-s;
+
+    /* Parse the symmetry type. */
+    if (strcmp("general", t) == 0 || strcmp("general\n", t) == 0) {
+        *symmetry = mtx_general;
+    } else if (strcmp("symmetric", t) == 0 || strcmp("symmetric\n", t) == 0) {
+        *symmetry = mtx_symmetric;
+    } else if (strcmp("skew-symmetric", t) == 0 ||
+               strcmp("skew-symmetric\n", t) == 0)
+    {
+        *symmetry = mtx_skew_symmetric;
+    } else if (strcmp("hermitian", t) == 0 || strcmp("hermitian\n", t) == 0 ||
+               strcmp("Hermitian", t) == 0 || strcmp("Hermitian\n", t) == 0)
+    {
+        *symmetry = mtx_hermitian;
+    } else {
+        return MTX_ERR_INVALID_MTX_SYMMETRY;
+    }
+    while (*t != '\0' && *t != '\n')
+        t++;
+    *bytes_read = t-s;
+
+    if (endptr)
+        *endptr = t;
+    return MTX_SUCCESS;
 }
