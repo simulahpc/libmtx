76e85ebdbb418be7b5b8133811fba74f5ed544d7
James D. Trotter <james@simula.no>
2022-10-04 07:39:32 +0200

    linalg/local: fix include header guard

--- libmtx/linalg/local/vector.h
+++ libmtx/linalg/local/vector.h
@@ -16,13 +16,13 @@
  * along with Libmtx.  If not, see <https://www.gnu.org/licenses/>.
  *
  * Authors: James D. Trotter <james@simula.no>
- * Last modified: 2022-07-11
+ * Last modified: 2022-10-03
  *
  * Data structures for vectors.
  */
 
-#ifndef LIBMTX_VECTOR_VECTOR_H
-#define LIBMTX_VECTOR_VECTOR_H
+#ifndef LIBMTX_LINALG_LOCAL_VECTOR_H
+#define LIBMTX_LINALG_LOCAL_VECTOR_H
 
 #include <libmtx/libmtx-config.h>
 
